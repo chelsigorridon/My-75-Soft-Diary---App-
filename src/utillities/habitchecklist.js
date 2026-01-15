14750f272f5cc41877c1ee9ecc9cb20ebf1e0f0c
chelsigorridon <chelsijaimegorridon@gmail.com>
2026-01-15 21:32:36 +0200

    Created example Array for checklist

---
+++ src/utillities/habitchecklist.js
@@ -0,0 +1,8 @@
+const habit =[
+    {id : 1, name: "Drink Water", type: "good", frequency: "Daily", completed: false},
+    {id : 2, name: "Exercise", type: "good", frequency: "Daily" , completed: false},
+    {id : 3, name: "Read a Book", type: "good", frequency: "Daily" , completed: false},
+    {id : 4, name: "Meditate", type: "good", frequency: "Daily" , completed: false},
+    {id : 5, name: "Sleep Early",type: "good", frequency: "Daily" , completed: false}
+    
+]
